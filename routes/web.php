e59bbea7aeceaa4a4218a1cbd3cd86697ac24ce9
LuisDonaldo2003 <l21930214@cdaltamirano.tecnm.mx>
2023-11-13 16:38:16 -0600

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,47 @@
-<?php
+    <?php
 
-use App\Http\Controllers\ActivityController;
-use App\Http\Controllers\PeriodController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\RegistrationController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Session;
+    use App\Http\Controllers\ActivityController;
+    use App\Http\Controllers\PeriodController;
+    use App\Http\Controllers\ProfileController;
+    use App\Http\Controllers\RegistrationController;
+    use App\Http\Controllers\UserController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Auth;
+    use Illuminate\Support\Facades\Hash;
+    use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Validator;
+    use Illuminate\Support\Facades\URL;
+    use Illuminate\Support\Facades\Session;
+    use PharIo\Manifest\Email;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "web" middleware group. Make something great!
+    |
+    */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 
-Route::resource('registrations', RegistrationController::class);
-
-Route::resource('activities', ActivityController::class);
-
-Route::resource('periods', PeriodController::class);
+    Route::resource('registrations', RegistrationController::class);
 
 
 
-require __DIR__.'/auth.php';
+
+    require __DIR__.'/auth.php';
